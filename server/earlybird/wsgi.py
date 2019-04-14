ebdd3f329901abe177b82ffa722209ff20c5d86f
eadu <eadu10261@gmail.com>
2019-04-14 15:45:48 -0400

    Changed from python 2.7 to python3

--- server/earlybird/wsgi.py
+++ server/earlybird/wsgi.py
@@ -6,17 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-#!/usr/bin/python
+#!/usr/bin/python3
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "earlybird.settings")
 
+from earlybird import web_scraper
+#web_scraper.run_check()
+
 application = get_wsgi_application()
-
-
-import subprocess 
-p = os.getcwd()
-a = "/web/groups/earlybirdsystem/public_html/process_tasks.sh"
-#subprocess.call(a)
